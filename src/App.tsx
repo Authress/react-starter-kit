678dcc4336cd179bf4d57526fc947deb8916bfb3
Benn Bollay <bb.github@magitech.org>
2023-03-13 15:44:08 -0700

    Add prettier and some doc changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import authressLogo from './assets/logo.svg'
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import authressLogo from './assets/logo.svg';
 import { LoginClient } from 'authress-login';
 
-let loginClient;
+let loginClient: LoginClient;
 try {
   loginClient = new LoginClient({
     // app_default is the default Authress created application.
@@ -14,23 +14,22 @@ try {
 
     // Create a custom domain: https://authress.io/app/#/settings?focus=domain (https://login.application.com)
     // * OR use the default one for your account: https://authress.io/app/#/api?route=overview (https://ACCOUNT_ID.api-region.authress.io)
-    authressLoginHostUrl: ''
+    authressLoginHostUrl: '',
   });
 } catch (error) {
   console.error(error);
 }
 
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [userProfile, setUserProfile] = useState({});
   const [authressLoginHostUrlIsSet, setAuthressLoginHostUrlIsSet] = useState(true);
-  
+
   // let userProfile;
   useEffect(() => {
     if (loginClient) {
-      loginClient.userSessionExists().then(userIsLoggedIn => {
+      loginClient.userSessionExists().then((userIsLoggedIn) => {
         setUserProfile(loginClient.getUserIdentity());
         console.log('User is Logged In', userIsLoggedIn, userProfile);
       });
@@ -52,38 +51,55 @@ function App() {
         </a>
       </div>
       <h1>Authress + React</h1>
-      <div style={{ 'border': '1px white solid', 'borderRadius': '10px', 'padding': '2rem' }}>
-        { authressLoginHostUrlIsSet ?
-          (<div>
-            <button style={{ 'marginRight': '1rem' }} onClick={login}>Login</button>
-            <button style={{ 'marginRight': '1rem' }} onClick={logout}>Logout</button>
-            <button style={{ 'marginRight': '1rem' }} onClick={redirectToGuardedPage}>Go to Guarded Page</button>
-            <button style={{ 'marginRight': '1rem' }} onClick={makeApiCallWithUserToken}>Call your service API</button>
+      <div
+        style={{
+          border: '1px white solid',
+          borderRadius: '10px',
+          padding: '2rem',
+        }}
+      >
+        {authressLoginHostUrlIsSet ? (
+          <div>
+            <button style={{ marginRight: '1rem' }} onClick={login}>
+              Login
+            </button>
+            <button style={{ marginRight: '1rem' }} onClick={logout}>
+              Logout
+            </button>
+            <button style={{ marginRight: '1rem' }} onClick={redirectToGuardedPage}>
+              Go to Guarded Page
+            </button>
+            <button style={{ marginRight: '1rem' }} onClick={makeApiCallWithUserToken}>
+              Call your service API
+            </button>
 
-            <br></br><br></br>
+            <br></br>
+            <br></br>
 
-            <div style={{ "textAlign": "left" }}>
+            <div style={{ textAlign: 'left' }}>
               <span>User Profile:</span>
-              <pre>{ JSON.stringify(userProfile || 'Not logged in', null, 2) }</pre>
+              <pre>{JSON.stringify(userProfile || 'Not logged in', null, 2)}</pre>
             </div>
 
-            <br></br><br></br>
+            <br></br>
+            <br></br>
             <div>
               Edit <code>src/App.tsx</code> to update the login function.
             </div>
-          </div>)
-          : (<div style={{ color: '#dc3545' }}>
-              <div>
-                Authress Account Host URL is missing from your configuration. Go to <strong>src/App.tsx</strong> and specify the <strong>AuthressLoginHostUrl</strong> at the top of the file..
-              </div>    
-            </div>)
-        }
+          </div>
+        ) : (
+          <div style={{ color: '#dc3545' }}>
+            <div>
+              Authress Account Host URL is missing from your configuration. Go to <strong>src/App.tsx</strong> and
+              specify the <strong>AuthressLoginHostUrl</strong> at the top of the file. <br />
+              If you need an account, sign up for free at <a href="https://authress.io/app/#/signup">Authress.io</a>!
+            </div>
+          </div>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Authress logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Authress logos to learn more</p>
     </div>
-  )
+  );
 }
 
 async function login() {
@@ -95,7 +111,7 @@ async function login() {
 async function logout() {
   console.log('Logging out');
 
-  await loginClient.logout();
+  await loginClient.logout('http://localhost:8080');
 }
 
 async function redirectToGuardedPage() {
@@ -107,12 +123,12 @@ async function redirectToGuardedPage() {
 }
 
 async function makeApiCallWithUserToken() {
-  const userAccessToken = await loginClient.ensureToken();
+  const userAccessToken = await loginClient.ensureToken({});
   try {
     const result = await fetch('http://localhost:8080/api/endpoint', {
       headers: {
-        Authorization: `Bearer ${userAccessToken}`
-      }
+        Authorization: `Bearer ${userAccessToken}`,
+      },
     });
     console.log('API Call request', result);
   } catch (error) {
